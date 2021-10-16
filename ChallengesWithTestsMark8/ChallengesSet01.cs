14fbd74161386d196982832b71ebe13e50f4961c
tiagogomesgh <tiagogomesfl@outlook.com>
2021-10-15 21:40:54 -0400

    finished exc 1

---
+++ ChallengesWithTestsMark8/ChallengesSet01.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Linq;
+
+namespace ChallengesWithTestsMark8
+{
+    public class ChallengesSet01
+    {
+        public bool AreTwoNumbersTheSame(int num1, int num2)
+        {
+            if(num1 == num2)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public double Subtract(double minuend, double subtrahend)
+        {
+            var expectedDifference = minuend - subtrahend;
+            return expectedDifference;
+        }
+
+        public int Add(int number1, int number2)
+        {
+            int result = number1 + number2;
+            return result;
+            
+        }
+
+        public int GetSmallestNumber(int number1, int number2)
+        {
+            int[] smallest = { number1, number2 };
+            int result = Math.Min(number1, number2);
+            return result;
+            
+        }
+
+        public long Multiply(long factor1, long factor2)
+        {
+            long result = factor1 * factor2;
+            return result;
+        }
+
+        public string GetGreeting(string nameOfPerson)
+        {
+
+            string name = nameOfPerson;
+
+            var greeting = "Hello, " + name + "!";
+            var emptyGreet = "Hello!";
+
+            if (name == "")
+            {
+                return emptyGreet;
+            }
+            else
+            {
+                return greeting;
+            }
+            
+
+            
+        }
+
+        public string GetHey()
+        {
+
+            string hey = "HEY!";
+                return hey;
+
+
+        }
+    }
+}
---
+++ ChallengesWithTestsMark8/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ChallengesWithTestsMark8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Please Enter your Name.");
+            //ChallengesSet01 myChallengesSet01 = new ChallengesSet01();
+            //myChallengesSet01.AreTwoNumbersTheSame(2, 2);
+            //myChallengesSet01.Subtract(2, 2);
+            //myChallengesSet01.Add(2, 4);
+            //myChallengesSet01.GetSmallestNumber(1, 2);
+            //myChallengesSet01.Multiply(4, 4);
+            //myChallengesSet01.GetGreeting(Console.ReadLine());
+            Console.WriteLine();
+            
+        }
+    }
+}
